b830308d8efcd77feb456ebff9bfbcda0d8eafb7
Macbook <macbook@Catherines-MBP.lan>
2024-05-19 20:31:57 -0400

    Start set up App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+let getAlbums = async () => {
+  try {
+    let response = await fetch("http://localhost:4000/albums")
+    let data = await response.json()
+    console.log(data)
+  } catch(error) {
+    console.log(error)
+  }
+
+}
+
+let createAlbum = async () => {
+
+}
+
+useEffect (()=> {
+  getAlbums()
+
+}, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <h1>Welcome to Music World</h1>
+      <div></div>
+    </div>
+
   )
 }
 
---
+++ server/index.js
@@ -0,0 +1,52 @@
+
+const express = require('express')
+
+const app = express()
+
+const port = 4000;
+
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+    res.json({ info: 'This is a Node/Express App'})
+})
+
+app.post('/albums', (req, res)=>{
+    
+
+    let {author, title} = req.body
+    
+    if(author && title) {
+        console.log(`Creating a new album! ${title} by ${author}`)
+        res.json({id: 1, title, author})
+
+    } else {
+        console.log("Error creating album!")
+        res.status(400).send("Error creating album! Albums need a title and author")
+    }
+})
+
+
+app.get('/albums', (req, res)=>{
+    let data = [
+        {
+            author: "Tupac",
+            title: "All Eyez On Me"
+        }, 
+        {
+            author: "Young Jeezy",
+            title: "TM 101"
+        },
+        {
+            author: "Nipsey Hussle",
+            title: "Victory Lap"
+        }
+    ]
+    res.json(data)
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`)
+})
